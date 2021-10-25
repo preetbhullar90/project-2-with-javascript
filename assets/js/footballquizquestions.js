25c6a9059b089c2ba48bce7c6d6049c13d11307b
preetbhullar90 <gurpreet.singhb90@gmail.com>
2021-10-25 16:52:23 +0000

    add all the questions in js file

---
+++ assets/js/footballquizquestions.js
@@ -0,0 +1,56 @@
+
+const questions = [
+    {
+        q: '1. Which team has won the most FA Cup trophies?',
+        choices: ['Arsenal', 'Manchester United', 'Chelsea', 'Liverpool'],
+        answer: 0,
+
+
+
+    },
+    {
+        q: 'Which football national team won the 2014 world cup ?',
+        choices: ['England ', 'Germany ', 'France', 'Spain '],
+        answer: 1,
+    },
+    {
+        q: '3. Who has the most trophies in football history?',
+        choices: ['Xavi', 'Cristiano Ronaldo ', 'Lionel Messi', 'Dani Alves '],
+        answer: 3,
+    },
+    {
+        q: '4. Which player has the most goals?',
+        choices: ['Luis Suarez', 'Cristiano Ronaldo', 'Lionel Messi', 'Pele'],
+        answer: 1,
+    },
+    {
+        q: '5. Which player has the most assist in football history ?',
+        choices: ['Lionel Messi', 'Luis Suarez', 'Cristiano Ronaldo', 'Angel di maria'],
+        answer: 0,
+    },
+    {
+        q: 'Who has won the most premier league titles in history?',
+        choices: ['Liverpool', 'Manchester City', 'Manchester United', 'Leicester City'],
+        answer: 2,
+    },
+    {
+        q: '7. Which club has won the most champions league titles?',
+        choices: ['Real Madrid', 'Liverpool', 'Chelsea', 'Barcelona'],
+        answer: 0,
+    },
+    {
+        q: "Who has won the most ballon d'ors in football history?",
+        choices: ['Luka Modric', 'Lionel Messi', 'Zinedine Zidane', 'Cristiano Ronaldo '],
+        answer: 1,
+    },
+    {
+        q: 'Who has been given most red cards Since 2000?',
+        choices: ['Zlatan Ibrahimovic', 'Dani Alves', 'Pepe', 'Sergio Ramos'],
+        answer: 3,
+    },
+    {
+        q: 'who scored the most goals in a single season ?',
+        choices: ['Gerd muller', 'Cristian Ronaldo ', 'Lionel Messi', 'Arthur ceuleers '],
+        answer: 2,
+    }
+]
