27b199aa72ebf3e3c9cde22de3a3d74712342671
preetbhullar90 <gurpreet.singhb90@gmail.com>
2021-11-10 21:00:13 +0000

    Change few questions in js file

--- assets/js/footballquizquestions.js
+++ assets/js/footballquizquestions.js
@@ -1,7 +1,7 @@
 
 const questions = [
     {
-        q: 'Which team has won the most FA Cup trophies?',
+        q: 'Which football team has won the most FA Cup trophies?',
         choices: ['Arsenal', 'Manchester United', 'Chelsea', 'Liverpool'],
         answer: 0,
 
@@ -9,7 +9,7 @@ const questions = [
 
     },
     {
-        q: 'Which football national team won the 2014 world cup ?',
+        q: 'Which football national team won the 2014 FIFA world cup ?',
         choices: ['England ', 'Germany ', 'France', 'Spain '],
         answer: 1,
     },
@@ -19,12 +19,12 @@ const questions = [
         answer: 3,
     },
     {
-        q: 'Which player has the most goals in a calendar year?',
+        q: 'Which football player has scored the most goals in a calendar year?',
         choices: ['Luis Suarez', 'Cristiano Ronaldo', 'Lionel Messi', 'Pele'],
         answer: 1,
     },
     {
-        q: 'Which player has the most assists in football history?',
+        q: 'Which football player has the most assists in football history?',
         choices: ['Lionel Messi', 'Luis Suarez', 'Cristiano Ronaldo', 'Angel di maria'],
         answer: 0,
     },
@@ -34,7 +34,7 @@ const questions = [
         answer: 2,
     },
     {
-        q: 'Which club has won the most champions league titles?',
+        q: 'Which football club has won the most champions league titles?',
         choices: ['Real Madrid', 'Liverpool', 'Chelsea', 'Barcelona'],
         answer: 0,
     },
@@ -44,12 +44,12 @@ const questions = [
         answer: 1,
     },
     {
-        q: 'Who has been given the most red cards since 2000?',
+        q: 'Who has been given the most red cards since the year 2000?',
         choices: ['Zlatan Ibrahimovic', 'Dani Alves', 'Pepe', 'Sergio Ramos'],
         answer: 3,
     },
     {
-        q: 'Who scored the most goals in a single season ?',
+        q: 'Who scored the most goals in a single football season ?',
         choices: ['Gerd muller', 'Cristiano Ronaldo ', 'Lionel Messi', 'Arthur ceuleers '],
         answer: 2,
     }
